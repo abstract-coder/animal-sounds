bfc96a21616d4af7608bcb6f308ab96f9626137a
ishita221209 <84296284+ishita221209@users.noreply.github.com>
2022-10-10 16:44:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function model_loaded() {
     classfier.classify(got_results);
 }
 
-var dog = 0;
+var duck = 0;
 var cat= 0;
 var cow= 0;
 var pig= 0;
@@ -31,18 +31,26 @@ function got_results(error,results){
         if(results[0].label == "Cat"){
             document.getElementById("animal-pic").src= "cat.png";
             cat= cat+1;
+            document.getElementById("score").innerText= "Cat: "+cat+ " Pig: " + pig + " Duck: "+ duck+ " Cow: " + cow;
         }
         else if(results[0].label == "Pig"){
             document.getElementById("animal-pic").src= "pig.png";
             pig= pig+1;
+            document.getElementById("score").innerText= "Cat: "+cat+ " Pig: " + pig + " Duck: "+ duck+ " Cow: " + cow;
+
+
         }
         else if(results[0].label == "Duck"){
             document.getElementById("animal-pic").src= "Cute-Duck-PNG.png";
             duck= duck+1;
+            document.getElementById("score").innerText= "Cat: "+cat+ " Pig: " + pig + " Duck: "+ duck+ " Cow: " + cow;
+
         }
         else if(results[0].label == "Cow"){
             document.getElementById("animal-pic").src= "cow.png";
             cow= cow+1;
+            document.getElementById("score").innerText= "Cat: "+cat+ " Pig: " + pig + " Duck: "+ duck+ " Cow: " + cow;
+
         }
         else{
             document.getElementById("animal-pic").src= "listen.png";
